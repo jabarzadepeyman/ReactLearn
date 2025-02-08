94a7a2a4b24bd7a2995cf1a6aaffb43bd75736ae
Peyman Jabarzadeh <peyman@example.com>
2025-02-08 13:55:35 +0330

    اضافه کردن فایل hoisting Training.js

--- Export in Js.js
+++ Export in Js.js
@@ -1,1 +1,19 @@
-let name = 'ali'
+// named export
+export let name = 'ali'
+export function greet(name) {
+    console.log(`hi ${name}`);
+}
+
+
+// object export
+let family = "Josefin";
+function seyHi (){
+    console.log('hi')
+}
+export {family, seyHi};
+
+// default
+// اگر فقط همین یدونه هست که میخوای خروجی بگری حالا چه متغیر چه فانکش این روش توصیه میشه
+export default function dude() {
+    console.log(`hi dude`);
+}
---
+++ import in Js.js
@@ -0,0 +1,9 @@
+// Named Import
+import {name,family} from "./Export in Js";
+
+// default import
+// چون داخل اون یک کامپننت هست میتونی هر اسمی که میخوای رو بزاری
+import salamdude from "./Export in Js";
+
+// میتونی موقع امپرت کردن اسم دلخواهت رو بزار
+import {name as lahgab,family as namekhanevadegi} from "./Export in Js";
